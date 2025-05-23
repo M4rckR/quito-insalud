517115fec8c0eb1e2cf8e0ff701acf1c7e70d91c
m4arck <marcromerogar4@gmail.com>
2025-05-23 11:11:48 -0500

    Actualizacion de clases

--- src/components/ui/ButtonWspItem.tsx
+++ src/components/ui/ButtonWspItem.tsx
@@ -1,6 +1,6 @@
 export const ButtonWspItem = () => {
   return (
-    <div>
+    <div className="btn-wsp-quito">
         <a className="fixed bottom-1/12 w-12 h-12  right-4 z-50  rounded-full shadow-lg transition-transform duration-300 hover:scale-105" href="https://wa.link/ur5m3o" target="_blank"> 
             <img width={10} height={10} className="w-12" src="icons/button-wsp.svg" alt="icono whatsapp" />
         </a>
